1bbabe7cc90fcd24177c8c98b5bb920be33c370c
cherryzp <win9101@gmail.com>
2018-06-23 16:10:20 +0900

    First commit

--- TeamProject/index.js
+++ TeamProject/index.js
@@ -1,19 +1,32 @@
 $(document).ready(function(){
-	$('#main_target').load('mobile_home.html');
+	$('#main_target').load('main.html');
 
 	$('#mobile_hidden_list').hide();
 
 	$('#mobile_menu_icon').on('click', function(){
-    	$('#mobile_hidden_list').toggle();
+    	$('#mobile_hidden_list').slideToggle("fast");
     	return false;
     });
 
-    $('#rank_btn').on('click', function(){
-    	$('#main_target').load('Genre/rank.html')
+    $('.rank_btn').on('click', function(){
+    	$('#main_target').load('rank.html');
+        $('#mobile_hidden_list').slideUp("fast");
     	return false;
     });
 
-    $('#home_btn').on('click', function(){
+    $('.boxoffice_btn').on('click', function(){
+        $('#main_target').load('boxoffice.html');
+        $('#mobile_hidden_list').slideUp("fast");
+        return false;
+    });
+
+    $('.home_btn').on('click', function(){
     	$('#main_target').load('mobile_home.html');
+        $('#mobile_hidden_list').slideUp("fast");
     });
+
+    $('.magazine_btn').on('click', function(){
+        $('#main_target').load('news.html')
+    });
+
 });
